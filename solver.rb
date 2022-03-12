4863499eb52b264584847456d086295e3dc1ffe9
lily-coder <lilyondie@gmail.com>
2022-03-12 07:47:45 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,33 +1,33 @@
 class Solver
-    def factorial(n)
-      raise StandardError, 'Please input numbers starting from zero or positive integers only' if n.negative?
-  
-      result = 1
-  
-      while n.positive?
-        result *= n
-        n -= 1
-      end
-  
-      result
+  def factorial(num)
+    raise StandardError, 'Please input numbers starting from zero or positive integers only' if num.negative?
+
+    result = 1
+
+    while num.positive?
+      result *= num
+      num -= 1
     end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(n)
-      div_one = n.modulo(3).zero?
-      div_two = n.modulo(5).zero?
-  
-      if div_one && div_two
-        'fizzbuzz'
-      elsif div_two
-        'buzz'
-      elsif div_one
-        'fizz'
-      else
-        n.to_s
-      end
+
+    result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    div_one = num.modulo(3).zero?
+    div_two = num.modulo(5).zero?
+
+    if div_one && div_two
+      'fizzbuzz'
+    elsif div_two
+      'buzz'
+    elsif div_one
+      'fizz'
+    else
+      num.to_s
     end
   end
+end
